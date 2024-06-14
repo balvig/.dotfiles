d8f3a6fe9cde5737cfa8c2f409c116e36073c206
Jens Balvig <jens@balvig.com>
2024-06-14 14:14:38 +0900

    slim syntax highlighting

--- nvim/lua/plugins/auto-save.lua
+++ nvim/lua/plugins/auto-save.lua
@@ -4,8 +4,9 @@ return {
     cmd = "ASToggle", -- optional for lazy loading on command
     event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
     opts = {
-      -- your config goes here
-      -- or just leave it empty :)
+      execution_message = {
+        enabled = false,
+      },
     },
   },
 }
