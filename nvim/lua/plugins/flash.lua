77ab4b70f63ea8f377b76ad5631e7cbb5ac9a5bf
Jens Balvig <jens@balvig.com>
2024-06-26 13:30:12 +0900

    Disable flash for f

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/flash.nvim",
+  opts = {
+    modes = {
+    -- disable flash for
+    -- `f`, `F`, `t`, `T`, `;` and `,` motions
+      char = {
+        enabled = false
+        -- autohide = true
+      }
+    }
+  },
+}
