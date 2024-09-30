8d7c9f96afb5f47cffa65ee23c34c13318774bfa
Jens Balvig <jens@balvig.com>
2024-09-30 12:17:01 +0900

    Better contrast for line numbers

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,6 +1,16 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
   { "Mofiqul/dracula.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      require("tokyonight").setup({
+        on_colors = function(colors)
+          colors.fg_gutter = "#627E97"  -- Clearer line numbers
+        end,
+      })
+    end
+  },
 
   {
     "LazyVim/LazyVim",
