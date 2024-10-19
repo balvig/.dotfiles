a84cdf76fd9d87962cdb680565947e080252fe86
Jens Balvig <jens@balvig.com>
2024-10-19 20:26:56 +0900

    Nicer icons for neotest

---
+++ nvim/lua/plugins/neotest.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-neotest/neotest",
+  opts = {
+    icons = {
+      expanded = "",
+      child_prefix = "",
+      child_indent = "",
+      final_child_prefix = "",
+      non_collapsible = "",
+      collapsed = "",
+
+      passed = "",
+      running = "",
+      failed = "",
+      unknown = ""
+    }
+  }
+}
