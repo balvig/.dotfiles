6a2f6f09f6766385f7ee823d8b5f654e546ce49b
Jens Balvig <jens@balvig.com>
2024-11-26 10:43:14 +0900

    Reduce list of colorschemes

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,3 +1,17 @@
+-- Ignore built-in colorschemes
+local builtins = {}
+
+for name, _ in vim.fs.dir("$VIMRUNTIME/colors/") do
+  if name:match("*.txt") then
+    break
+  else
+    name = name:match(".*%.lua$") or name:match(".*%.vim$")
+    table.insert(builtins, name)
+  end
+end
+
+vim.opt.wildignore:append(builtins)
+
 return {
   { "ellisonleao/gruvbox.nvim" },
   { "Mofiqul/dracula.nvim" },
