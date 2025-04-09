29462ab47a5f33fbb3eb00cc7a4c8f0c81b4ea32
Jens Balvig <jens@balvig.com>
2025-04-09 11:45:11 +0900

    Using later versions of Node now

--- nvim/lua/plugins/copilot.lua
+++
@@ -1,9 +0,0 @@
-  -- Point copilot to compatible Node version independent from current project
-return {
-  {
-    "zbirenbaum/copilot.lua",
-    opts = {
-      copilot_node_command = vim.fn.expand("$HOME") .. "/.nvm/versions/node/v18.20.3/bin/node"
-    },
-  }
-}
