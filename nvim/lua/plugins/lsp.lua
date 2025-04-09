60eeb999a6f8f0a93606391b4fe9d8fdd50e0476
Jens Balvig <jens@balvig.com>
2025-04-09 11:44:16 +0900

    Fix Rubocop issues

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,3 +1,5 @@
+local lspconfig = require("lspconfig")
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -6,6 +8,11 @@ return {
         ruby_lsp = {
           mason = false,
           cmd = { vim.fn.expand("~/.asdf/shims/ruby-lsp") },
+        },
+        rubocop = {
+          mason = false,
+          cmd = { "bundle", "exec", "rubocop", "--lsp" },
+          root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
         },
       },
     },
