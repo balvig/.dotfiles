9654ebcef2abe30981c29452b9b27f6712e9e834
Jens Balvig <jens@balvig.com>
2025-08-07 12:27:14 +0200

    Fix syntax highlighting for eruby.yaml files

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Syntax highlighting for eruby files with yaml extension
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "eruby.yaml",
+  command = "set filetype=yaml",
+})
