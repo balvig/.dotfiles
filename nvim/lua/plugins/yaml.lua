35315a889f0aa1f418deb5b216878babf76994f6
Jens Balvig <jens@balvig.com>
2025-08-30 11:29:41 +0900

    Assign yaml yank to shortcut

--- nvim/lua/plugins/yaml.lua
+++ nvim/lua/plugins/yaml.lua
@@ -4,4 +4,12 @@ return {
     "nvim-treesitter/nvim-treesitter",
     "nvim-telescope/telescope.nvim", -- optional
   },
+  ft = { "yaml", "yml" },
+  keys = {
+    {
+      "<leader>yy", -- pick any shortcut you like
+      "<cmd>YAMLYankKey *<cr>",
+      desc = "Yank YAML Key",
+    },
+  },
 }
